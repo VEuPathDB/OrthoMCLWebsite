698f5f71bab859693cca84c096850cd764997ba8
Steve Fischer <sfischer@upenn.edu>
2007-12-06 15:39:45 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebsite/trunk@19473 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/cgi-lib/PpeProcessor.pm
+++ Site/cgi-lib/PpeProcessor.pm
@@ -19,3 +19,26 @@ sub createMatrixTable {
   return if matrixTableExists();
 
 }
+
+sub parseCladeTreeFile() {
+    my ($cladeTreeFile) = @_;
+
+    open(FILE, $cladeTreeFile) || die "couldn't open cladeTreeFile '$cladeTreeFile'\n";
+    my $handle = ;
+    my $clade = {};
+    my $indent = 0;
+    &descendCladeTreeFile($handle, $tree, $indent)
+}
+
+sub descendCladeTreeFile() {
+    my ($handle, $clade, $indent) = @_;
+
+    my $line = $handle->nextLine();
+    if ($line =~ /([A-Z][A-Z][A-Z])/) {  # new clade
+	
+	$clade->();
+    } elsif ($line =~ /([a-z][a-z][a-z])/) {
+    } else {
+	die "invalid input line\n";
+    }
+}

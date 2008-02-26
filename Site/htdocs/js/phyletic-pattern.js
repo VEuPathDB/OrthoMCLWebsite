d26ab01e5f49d321ffa8f05cb552f11f299c1430
Xin Gao <xingao@upenn.edu>
2008-02-26 18:40:46 +0000

    adjust phyletic pattern color
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebsite/trunk@20557 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/js/phyletic-pattern.js
+++ Site/htdocs/js/phyletic-pattern.js
@@ -98,7 +98,7 @@ function displayLegend() {
         for (var col = 0; col < categories[row].length; col++) {
             var category = categories[row][col];
             
-            content.push("<td><div index=\"", index ,"\" count=\"many\" ");
+            content.push("<td><div index=\"", index ,"\" count=\"0\" ");
             content.push(" style=\"width: 30px;\" ");
             content.push(" onmouseover=\"return escape(getLegendDetail(", category.root.id, "));\">");
             content.push(category.root.abbrev, "</div></td>");

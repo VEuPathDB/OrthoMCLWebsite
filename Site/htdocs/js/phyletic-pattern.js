ba677625992b32e4d07ff3f88a69007b5f1d6df1
Xin Gao <xingao@upenn.edu>
2009-08-17 20:21:19 +0000

    update display with collapsible branches.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebsite/trunk@30853 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/js/phyletic-pattern.js
+++ Site/htdocs/js/phyletic-pattern.js
@@ -1,167 +1,122 @@
-var taxons = { };
-var categories = [];
+$(document).ready(function() {
+    GroupDisplay display = new GroupDisplay();
+    display.buildTree();
+    display.createLayout();
+    display.fillCounts();
+});
 
-function initial() {
-    var taxonMap = { };
+function TreeNode(id, parentId, isLeaf, abbrev, index, name) {
+    var this.parent == null;
+    var this.id = id;
+    var this.parentId = parentId;
+    var this.isLeaf = isLeaf;
+    var this.index = index;
+    var this.name = name;
+    var this.children = new Array();
     
-    // resolve the children of each node
-    for (var taxon_id in taxons) {
-        var taxon = taxons[taxon_id];
-        taxonMap[taxon.abbrev] = taxon;
-        if (taxon_id != taxon.parent_id) {
-            var parent = taxons[taxon.parent_id];
-            parent.children.push(taxon);
-        }
-        // modify display names
-        if (taxon.abbrev == "EUKA" || taxon.abbrev == "BACT") 
-            taxon.name = "Other " + taxon.name;
-            
-        getPath(taxon);
-    }
-
-    // get categories as the children of roots
-    makeCategories(taxonMap);
-    
-    // order the species in each category
-    for (var row = 0; row < categories.length; row++) {
-        for (var col = 0; col < categories[row].length; col++) {
-            var category = categories[row][col];
-            category.species.sort(compareTaxons);
-        }
-    }
-}
-
-function getPath(taxon) {
-    if (!("path" in taxon)) {
-        if (taxon.id == taxon.parent_id) taxon.path = "";
-        else {
-            var parentPath = getPath(taxons[taxon.parent_id]);
-            if (parentPath != "") parentPath += "->";
-            taxon.path = parentPath + taxon.abbrev;
-        }
-    }
-    return taxon.path;
-}
-
-function makeCategories(taxonMap) {
-    // the order of each key in the keys array is important, and the parent 
-    // level is always after the children level in order to make correct groups
-    var keys = [["FIRM", 0, 1], ["PROT", 0, 2], ["BACT", 0, 0],
-                ["ARCH", 1, 0], 
-                ["PLAL", 1, 2], ["KINE", 1, 3], ["ALVE", 1, 4],
-                ["FUNG", 2, 0], ["META", 2, 1],
-                ["EUKA", 1, 1]];
-                
-    // prepare category array
-    for (var taxon_id in taxons) {
-        var taxon = taxons[taxon_id];
-        if (!taxon.is_species) continue;
-        
-        for (var i = 0; i < keys.length; i++) {
-            var key = keys[i][0];
-            var row = keys[i][1];
-            var col = keys[i][2];
-            
-            var category;
-            if (!categories[row] || !categories[row][col]) {
-                category = { root: taxonMap[key],
-                          species: [] };
-                if (!categories[row]) categories[row] = [];
-                categories[row][col] = category;
-            } else category = categories[row][col];
-            
-            if (isAncestor(taxon, category.root)) {
-                category.species.push(taxon);
-                taxon.category = category;
-                break;
+    this.getPath = function() {
+        if (!("path" in this)) {
+            if (this.id == this.parentId) this.path = "";
+            else {
+                var parentPath = parent.getPath();
+                if (parentPath != "") parentPath += "->";
+                this.path = parentPath + this.abbrev;
             }
         }
+        return this.path;
+    };
+}
+
+function GroupDisplay() {
+    var nodes = { };
+    var roots = new Array();
+
+    this.buildTree = function() {
+        $("#phyletic-data taxon").each(function() {
+            var taxonId = $(this).attr("taxon-id");
+            var parentId = $(this).attr("parent");
+            var abbrev = $(this).attr("abbrev");
+            var isLeaf = $(this).attr("leaf-node") == "1" ? true : false;
+            var index = $(this).attr("index");
+            var name = $(this).html();
+            var node = new TreeNode(taxonId, parentId, isLeaf, abbrev, index, name);
+            this.nodes[taxonId] = node;
+        });
+        for(var taxonId in this.nodes) {
+            var node = this.nodes[taxonId];
+            node.parent = this.nodes[node.parentId];
+            node.parent.children.push(node);
+        }
+        for (var taxonId in this.nodes) {
+            var node = this.nodes[taxonId];
+            if (node.parent == null) this.roots.push(node);
+        }
     }
-}
 
-function isAncestor(taxon, ancestor) {
-    while (taxon.id != taxon.parent_id) {
-        var parent = taxons[taxon.parent_id];
-        if (parent == ancestor) return true;
-        taxon = parent;
+    this.createLayout = function() {
+        var stub = $("#groups .group:first .phyletic-pattern");
+        for(var root in this.roots) [
+            var div = this.createNode(root);
+            stub.append(div);
+        }
+        var index = 0;
+        $("#groups .group .phyletic-pattern").each(function() {
+            if (index++ == 0) return;
+            $(this).append(stub.clone());
+        });
+        // register collapse events
+        $("#groups .group .phyletic-pattern .branch .handle").each(function() {
+            var parent = $(this).parent();
+            parent.children(".node:first").toggle();
+            parent.children(".children").toggle();
+            var img = $(this).attr("src");
+            img = (img.lastIndexOf("plus") > 0) ? "minus" : "plus";
+            $(this).attr("src", "images/" + img + ".gif");
+        }
     }
-    return false;
-}
-
-function compareTaxons(a, b) {
-    if(!(a.is_species ^ b.is_species)) return a.index - b.index;
-    else if (a.is_species) return -1;
-    else return 1;
-}
-
-function displayLegend() {
-    var content = [];
     
-    var index = 0;
-    for (var row = 0; row < categories.length; row++) {
-        for (var col = 0; col < categories[row].length; col++) {
-            var category = categories[row][col];
-            
-            content.push("<td><div index=\"", index ,"\" count=\"0\" location='head-tail'");
-            content.push(" style=\"width: 30px;\" ");
-            content.push(" onmouseover=\"return escape(getLegendDetail(", category.root.id, "));\">");
-            content.push(category.root.abbrev, "</div></td>");
-            
-            index++;
+    this.createNode = function(node) {
+        var div = "";
+        if (!node.isLeaf) {
+            div += "<div class=\"branch\">";
+            div += "<img class=\"handle\" src="images/plus.gif\" />";
+        }
+        div += "<div class=\"node\" taxon=\"" + node.taxonId + "\">";
+        div += "<span class=\"abbrev\">" + node.abbrev + "<span><br />";
+        div += "<span class=\"count\"></span>";
+        if (!node.isLeaf) {
+            div += "<div class=\"children\">"
+            for(var child in node.children) {
+                div += this.createNode(child);
+            }
+            div += "</div></div>";
+        }
+        return div;
+    }
+    
+    this.fillInCounts = function() {
+        $("#groups .group .phyletic-pattern").each(function () {
+            var counts = { };
+            $(this).find(".count-data group").each(function () {
+                var taxonId = $(this).attr("taxon-id");
+                var count = $(this).attr("count");
+                counts[taxonId] = count;
+            });
+            for (var taxonId in nodes) {
+                var count = this.getCount(node, counts);
+                $(this).find(".node[taxon=" + taxonId + "]").text(count);
+            }
+        });
+    }
+    
+    this.getCount = function(node, counts) {
+        if (node.isLeaf) return counts[node.taxonId];
+        else {
+            var count = 0;
+            for (var child in node.children) {
+                count += this.getCount(child, counts);
+            }
+            return count;
         }
     }
-
-    document.write(content.join(""));      
-}
-
-function getLegendDetail(taxon_id) {
-    var taxon = taxons[taxon_id];
-    var content = [];
-    content.push("<i>", taxon.name, "</i> (", taxon.abbrev, "), ", taxon.path);
-    return content.join("");
-}
-
-function displayCategories(group) {
-    var content = [];
-
-    var index = 0;
-    for (var row = 0; row < categories.length; row++) {
-        content.push("<tr>");
-        if (row == 2) content.push("<td></td><td></td>");
-        for (var col = 0; col < categories[row].length; col++) {
-            if (row == 1 && col == 1) content.push("<td></td>");
-            displayCategory(content, group, categories[row][col], index++);
-        }
-        content.push("</tr>");
-    }
-
-    document.write(content.join(""));      
-}
-
-function displayCategory(content, group, category, index) {
-    for(var i = 0; i < category.species.length; i++) {
-        var taxon = category.species[i];
-        var count = (taxon.id in group) ? group[taxon.id] : 0;
-        var position = "";
-        if (i == 0 && i == category.species.length - 1) position = "head-tail";
-        else if (i == 0) position = "head";
-        else if (i == category.species.length - 1) position = "tail";
-        
-        content.push("<td>");
-        
-        content.push("<div class='species' index='", index ,"' ");
-        if (position != "") content.push(" location='", position ,"' ");
-        content.push(" count='", ((count > 1) ? "many" : count) ,"' ");
-        content.push(" onmouseover=\"return escape(getTaxonDetail(", taxon.id, ", ", count, "));\">");
-        content.push(taxon.abbrev, "<br>", count, "</div></td>");
-    }
-}
-
-function getTaxonDetail(taxon_id, count) {
-    var taxon = taxons[taxon_id];
-    var content = [];
-    content.push("<i>", taxon.name, "</i> (", taxon.abbrev, ") = ", count, " gene");
-    if (count > 1) content.push("s");
-    content.push(", ", taxon.category.root.name, ", ", taxon.path);
-    return content.join("");
 }

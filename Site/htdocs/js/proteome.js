6024863ed00b95ecd2b3d53da337694fc1b9c07e
Xin Gao <xingao@upenn.edu>
2009-11-05 15:48:46 +0000

    add email format validation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebsite/trunk@32067 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/htdocs/js/proteome.js
+++ Site/htdocs/js/proteome.js
@@ -21,18 +21,30 @@ function Proteome() {
             var result = true;
             $(this).find("input").each(function() {
                 var required = $(this).attr("required");
-                if (required != "true") return;
-      
-                // reset the state
-                $(this).siblings(".error").remove();
-                $(this).css("background-color", "white");
+                var invalid = false;
                 var value = $(this).val();
-                if (value == "") {
-                    result = false;
-                    $(this).before("<div class=\"error\" style=\"color:red\">" + $(this).attr("name") + " is required.</div>");
-                    $(this).css("background-color", "#FFCCCC");
-                    $(this).focus();
+                if (required == "true")  {
+                    // reset the state
+                    $(this).siblings(".error").remove();
+                    $(this).css("background-color", "white");
+                    if (value == "") {
+                        result = false;
+                        invalid = true;
+                        $(this).before("<div class=\"error\" style=\"color:red\">" + $(this).attr("name") + " is required.</div>");
+                        $(this).css("background-color", "#FFCCCC");
+                        $(this).focus();
+                    }
                 }
+                if (!invalid && $(this).attr("name") == 'email') {
+                    // validate email
+                    var pattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+                    if (!pattern.test(value)) {
+                        result = false;
+                        $(this).before("<div class=\"error\" style=\"color:red\">email is invalid.</div>");
+                        $(this).css("background-color", "#FFCCCC");
+                        $(this).focus();
+                    }
+                } 
             });
             return result;
         });

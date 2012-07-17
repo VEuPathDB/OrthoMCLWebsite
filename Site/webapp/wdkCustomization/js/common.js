b234a6501c603be1ea33aee934549e207b1fc656
Dave Falke <dmfalke@gmail.com>
2012-07-17 19:48:37 +0000

    refs 9452
    Added contact us form submission handling, including basic email addree validation
    
    
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebsite/trunk@48842 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/common.js
+++ Site/webapp/wdkCustomization/js/common.js
@@ -1,5 +1,94 @@
 $(function() {
     $(".button").button();
+
+    // submit Contact us via AJAX
+    $("body").on("submit", "#contact-us", function(e) {
+      e.preventDefault();
+      var form = this,
+          jform = $(form),
+          emailRegex = /^\S+@\S+$/,
+          ccAddrs = this.addCc.value.split(/,\s*(?=\w)/);
+
+      // validation of email addresses
+      if (ccAddrs.length > 10) {
+        $("<div>You have entered more than 10 Cc addresses. " +
+            "Please limit this to 10.</div>").dialog({
+          title: "Oops! An error occurred.",
+          buttons: [{
+            text: "OK",
+            click: function() { $(this).dialog("close"); }
+          }],
+          modal: true
+        });
+        return;
+      }
+
+      var badAddrs = $.grep(ccAddrs.concat(form.reply.value), function(addr) {
+        emailRegex.test(addr);
+      }, true);
+
+      if (badAddrs.length > 0) {
+        var list = $.map(badAddrs, function(addr) {
+          return "<li>" + addr + "</li>";
+        }).join("");
+        $("<div></div>").html("<h3>The following email addresses appear to " +
+            "be invalid.</h3><ul>" + list + "</ul>").dialog({
+          title: "Oops! An error occurred.",
+          buttons: [{
+            text: "OK",
+            click: function() { $(this).dialog("close"); }
+          }],
+          modal: true
+        }).addClass("contact-us");
+        return;
+      }
+
+      // send request
+      $.post(jform.attr("action"), jform.serialize(), function(data) {
+        switch (data.status) {
+
+          case "success":
+            // close containing dialog
+            form.reset();
+            jform.parents("#wdk-dialog-contact-us").dialog("close");
+            $("<div></div>").html(data.message).dialog({
+              title: "Thank you!",
+              buttons: [{
+                text: "OK",
+                click: function() { $(this).dialog("close"); }
+              }],
+              modal: true
+            });
+            break;
+
+          case "error":
+            var response = "<h3>Please try to correct any errors below</h3>" +
+                "<br/>" + data.message;
+            $("<div></div>").html(response).dialog({
+              title: "Oops! An error occurred.",
+              buttons: [{
+                text: "OK",
+                click: function() { $(this).dialog("close"); }
+              }],
+              modal: true
+            });
+            break;
+        }
+      }, "json").error(function(jqXHR, textStatus, errorThrown) {
+        var response = "<h3>A " + textStatus + " error occurred.</h3><br/>" +
+            "<p>This indicates a problem with our server. Please email " +
+            "support directly.";
+        $("<div></div>").html(response).dialog({
+          title: "Oops! An error occurred.",
+          buttons: [{
+            text: "OK",
+            click: function() { $(this).dialog("close"); }
+          }],
+          modal: true
+        });
+      });
+    });
+
 });
 
 function login() {

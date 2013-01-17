c21c22955f1e5d5678aea5c5ddb36f41af4693b1
Dave Falke <dmfalke@gmail.com>
2013-01-17 20:03:02 +0000

    Add attachments to contact form refs #8406 refs #11079
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebsite/trunk@52776 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/common.js
+++ Site/webapp/wdkCustomization/js/common.js
@@ -4,12 +4,76 @@
 var Setup = {
 
   setUpContactUsLogic: function() {
+    var $form = $("#contact-us");
+
+    // add/remove files from form
+    $form.on("change", ":file", function(e) {
+
+      var $files = $(":file", $form);
+
+      if ($files.length >= 3) {
+        return;
+      }
+
+      var $container = $("#contact-files");
+
+      var addFile = function(idx) {
+        return $('<div><input type="file" name="attachment' + idx + '"/> </div>')
+        .appendTo($container);
+      };
+
+      // add additional file input, and add/remove links
+      $(" <a href='#'>remove</a>")
+      .click(function(e) {
+        e.preventDefault();
+
+        $(this).parent().remove();
+
+        var $resFiles = $(":file", $form)
+
+        if ($resFiles.length === 0) {
+          addFile(1);
+        }
+
+        $resFiles.each(function(idx, file) {
+          file.name = "attachment" + (idx + 1);
+        });
+
+      })
+      .insertAfter($(":file", $container).last());
+
+      if ($(":file", $form).length < 3) {
+        $("<a href='#'>add another file</a>")
+        .click(function(e) {
+          e.preventDefault();
+
+          var fileRow = addFile($files.length + 1);
+          $(this).remove();
+          $(":file", fileRow).click();
+        })
+        .insertAfter($container);
+      }
+    });
+
       // submit Contact us via AJAX
       $("body").on("submit", "#contact-us", function(e) {
         e.preventDefault();
         var form = this,
+            requiredFields = ['subject', 'reply', 'content'],
             emailRegex = /^\S+@\S+$/,
             ccAddrs = this.addCc.value.split(/,\s*(?=\w)/);
+
+        // validate required fields
+        var missingFields = _.filter(requiredFields, function(field) {
+          return !form[field].value;
+        });
+
+        if (missingFields.length) {
+          _.each(missingFields, function(field) {
+            $(form[field]).after("<span class='error'>This field is required</span>");
+          });
+          return;
+        }
 
         // validation of email addresses
         if (ccAddrs.length > 10) {
@@ -45,7 +109,12 @@ var Setup = {
           return;
         }
 
-        WdkAjax.sendContactRequest(form, function(data){
+        /*
+
+         we will submit form using the browser's native submit since
+         XHR can't handle file uploads
+
+        wdk.util.sendContactRequest(form, function(data){
           // close containing dialog
           form.reset();
           $(form).parents("#wdk-dialog-contact-us").dialog("close");
@@ -64,6 +133,11 @@ var Setup = {
               modal: true
             });
         });
+
+        */
+
+        form.submit();
+
       });
   },
 

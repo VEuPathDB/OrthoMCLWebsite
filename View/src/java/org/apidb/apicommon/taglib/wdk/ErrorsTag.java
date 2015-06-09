1ac5e1de72441a681c52cc9b3cbb3dd7bfae5e21
Ryan Doherty <tech@conical.org>
2015-06-09 13:28:29 +0000

    fix Java compiler warnings
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebsite/trunk@68262 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/src/java/org/apidb/apicommon/taglib/wdk/ErrorsTag.java
+++ View/src/java/org/apidb/apicommon/taglib/wdk/ErrorsTag.java
@@ -557,8 +557,7 @@ public class ErrorsTag extends WdkTagBase {
             "<not set; is 'JkEnvVar SERVER_ADDR' set in the Apache configuration?>";
         sb.append("Server Addr: " + serverAddr + "\n");
     }
-    
-    @SuppressWarnings("unchecked")
+
     private void appendRequestParameters(StringBuffer sb) {
         sb.append("Request Parameters (request to the server)\n\n");
         Map<String,String[]> parameters = request.getParameterMap(); 

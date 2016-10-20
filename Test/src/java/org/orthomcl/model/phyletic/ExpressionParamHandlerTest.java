515baebe59448d4c625d100373ec3cca286f2530
Ryan Doherty <tech@conical.org>
2016-10-20 02:38:45 +0000

    Remove secondary release method from WdkModel and use AutoCloseable interface to manage model resources across EuPathDB
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/OrthoMCLWebsite/trunk@77821 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Test/src/java/org/orthomcl/model/phyletic/ExpressionParamHandlerTest.java
+++ Test/src/java/org/orthomcl/model/phyletic/ExpressionParamHandlerTest.java
@@ -10,37 +10,41 @@ import org.junit.Test;
 
 public class ExpressionParamHandlerTest {
 
+  private static class ExpressionTester {
+
     private final User user;
     private final ExpressionParamHandler handler;
 
-    public ExpressionParamHandlerTest() throws WdkModelException {
-        String projectId = System.getProperty(Utilities.ARGUMENT_PROJECT_ID);
-        String gusHome = System.getProperty(Utilities.SYSTEM_PROPERTY_GUS_HOME);
-        WdkModel wdkModel = WdkModel.construct(projectId, gusHome);
-        user = wdkModel.getSystemUser();
-        handler = new ExpressionParamHandler();
-        handler.setWdkModel(wdkModel);
+    public ExpressionTester(WdkModel wdkModel) throws WdkModelException {
+      user = wdkModel.getSystemUser();
+      handler = new ExpressionParamHandler();
+      handler.setWdkModel(wdkModel);
     }
 
-    @Test
-    public void testTransform() throws WdkModelException {
-        testExpression("pviv = 2");
-        testExpression("pfal >3T");
-        testExpression("pber<= 5");
-        testExpression("pyoe+pkno>=4T");
-        testExpression("(pcha+PIRO)= 6");
-        testExpression("(BASI+ tann+bbov <7T)");
-        testExpression("COCC = 2 AND cmur>3T");
-        testExpression("tgon<= 2T or ncan= 4");
-        testExpression("cpar = 2 AND chom+ FUNG +ASCO>3T OR aory =4T");
-        testExpression("(ylip +spom = 2 and psti>3T) OR ncra =4T");
-        testExpression("((egos = 2 AND (cimm + cpos)>3T) Or (calb =4T And mgri>=5))");
-
+    public void test(String exp) throws WdkModelException {
+      System.out.println("Expression: " + exp);
+      String sql = handler.toInternalValue(user, exp, new HashMap<String,String>());
+      System.out.println("SQL: " + sql);
     }
+  }
 
-    private void testExpression(String exp) throws WdkModelException {
-        System.out.println("Expression: " + exp);
-        String sql = handler.toInternalValue(user, exp, new HashMap<String,String>());
-        System.out.println("SQL: " + sql);
+  @Test
+  public void testTransform() throws WdkModelException {
+    String projectId = System.getProperty(Utilities.ARGUMENT_PROJECT_ID);
+    String gusHome = System.getProperty(Utilities.SYSTEM_PROPERTY_GUS_HOME);
+    try (WdkModel wdkModel = WdkModel.construct(projectId, gusHome)) {
+      ExpressionTester tester = new ExpressionTester(wdkModel);
+      tester.test("pviv = 2");
+      tester.test("pfal >3T");
+      tester.test("pber<= 5");
+      tester.test("pyoe+pkno>=4T");
+      tester.test("(pcha+PIRO)= 6");
+      tester.test("(BASI+ tann+bbov <7T)");
+      tester.test("COCC = 2 AND cmur>3T");
+      tester.test("tgon<= 2T or ncan= 4");
+      tester.test("cpar = 2 AND chom+ FUNG +ASCO>3T OR aory =4T");
+      tester.test("(ylip +spom = 2 and psti>3T) OR ncra =4T");
+      tester.test("((egos = 2 AND (cimm + cpos)>3T) Or (calb =4T And mgri>=5))");
     }
+  }
 }

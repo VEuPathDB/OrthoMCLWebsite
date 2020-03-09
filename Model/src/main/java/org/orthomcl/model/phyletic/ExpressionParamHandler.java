78091e1efbd610ecc709dc5b3a30adeb3949f413
Mark Hickman <markhick@sas.upenn.edu>
2020-03-09 18:04:12 -0400

    change phyletic pattern query to match new orthomcl table

--- Model/src/main/java/org/orthomcl/model/phyletic/ExpressionParamHandler.java
+++ Model/src/main/java/org/orthomcl/model/phyletic/ExpressionParamHandler.java
@@ -20,12 +20,9 @@ public class ExpressionParamHandler extends StringParamHandler {
 
     private static final Logger LOG = Logger.getLogger(ExpressionParamHandler.class);
 
-    private static final String TAXON_SQL = "SELECT three_letter_abbrev "
-            + " FROM apidb.orthomcltaxon ORDER BY three_letter_abbrev";
-    private static final String GROUP_SQL = "SELECT ortholog_group_id "
-            + " FROM apidb.GroupTaxonMatrix";
-
-    private static final String COLUMN_PREFIX = "column";
+    private static final String GROUP_SQL = "SELECT ortholog_group_id FROM ( "
+            + "SELECT ortholog_group_id, sum(number_of_taxa) as number_of_taxa, sum(number_of_proteins) as number_of_proteins "
+	    + "FROM apidb.orthologgrouptaxon WHERE lower(three_letter_abbrev) IN (";
 
     private final ExpressionParser _parser = new ExpressionParser();
     private Map<String, Integer> _terms;
@@ -46,72 +43,41 @@ public class ExpressionParamHandler extends StringParamHandler {
             throws WdkModelException {
         LOG.debug("transforming phyletic param: " + stableValue);
 
-        _terms = getTerms();
-
-        // parse the expression and get the tree structure
+         // parse the expression and get the tree structure
         ExpressionNode root = _parser.parse(stableValue);
 
-        StringBuilder sql = new StringBuilder(GROUP_SQL);
-        sql.append(" WHERE " + composeSql(root));
+        String sql = composeSql(GROUP_SQL,root);
 
-        return sql.toString();
+	return sql;
     }
 
-    private Map<String, Integer> getTerms() throws WdkModelException {
-        Map<String, Integer> terms = new LinkedHashMap<String, Integer>();
-        DataSource dataSource = _wdkModel.getAppDb().getDataSource();
-        ResultSet resultSet = null;
-        try {
-            resultSet =
-                    SqlUtils.executeQuery(dataSource, TAXON_SQL,
-                            "ortho-exp-param-handler-taxon", 1000);
-            int order = 0;
-            while (resultSet.next()) {
-                String term = resultSet.getString(1);
-                terms.put(term.toLowerCase(), order++);
-            }
-        }
-        catch (SQLException ex) {
-            throw new WdkModelException(ex);
-        }
-        finally {
-            SqlUtils.closeResultSetAndStatement(resultSet, null);
-        }
-        return terms;
-    }
 
-    private String composeSql(ExpressionNode node) throws WdkModelException {
-        StringBuilder sql = new StringBuilder("(");
+    private String composeSql(String coreSql, ExpressionNode node) throws WdkModelException {
+        StringBuilder sql = new StringBuilder();
         if (node instanceof BooleanNode) {
             BooleanNode booleanNode = (BooleanNode) node;
-            sql.append(composeSql(booleanNode.getLeft()));
-            sql.append(" " + booleanNode.getOperator() + " ");
-            sql.append(composeSql(booleanNode.getRight()));
+            sql.append(composeSql(coreSql,booleanNode.getLeft()));
+	    if (booleanNode.getOperator().equals("and")) { sql.append(" INTERSECT "); }
+	    else if (booleanNode.getOperator().equals("or")) { sql.append(" UNION "); } 
+            sql.append(composeSql(coreSql,booleanNode.getRight()));
         }
         else {
+	    sql.append(coreSql);
             LeafNode leaf = (LeafNode) node;
             List<String> terms = leaf.getTerms();
-            if (terms.size() > 0) sql.append("(");
             StringBuilder sqlTerms = new StringBuilder();
             for (String term : terms) {
-                if (sqlTerms.length() > 1) sqlTerms.append(" + ");
-                String column = getColumn(term, leaf.isOnSpecies());
-                sqlTerms.append(column);
+		String species = "'" + term + "'";
+                if (sqlTerms.length() > 1) sqlTerms.append(",");
+                sqlTerms.append(species);
             }
-            sql.append(sqlTerms);
-            if (terms.size() > 0) sql.append(")");
-            sql.append(" " + leaf.getCondition() + leaf.getCount());
+            sql.append(sqlTerms + ")");
+	    sql.append(" GROUP BY ortholog_group_id) WHERE "); 
+	    if (leaf.isOnSpecies()) { sql.append("number_of_taxa"); }
+	    else { sql.append("number_of_proteins"); }
+	    sql.append(leaf.getCondition() + leaf.getCount());
         }
-        sql.append(")");
         return sql.toString();
     }
 
-    private String getColumn(String term, boolean onSpecies)
-            throws WdkModelException {
-        if (!_terms.containsKey(term))
-            throw new WdkModelException("Invalid expression. Unknown term: " + term);
-        int columnIndex = _terms.get(term) * 2 + 1;
-        if (onSpecies) columnIndex++;
-        return COLUMN_PREFIX + columnIndex;
-    }
 }
